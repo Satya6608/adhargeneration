2bde83da0eb96f07709baea2de8ef30d13ef80e5
Satya prakash <Satyaprakash6608@gmail.com>
2024-06-18 00:21:44 +0530

    text color change

--- src/components/AdharCard.jsx
+++ src/components/AdharCard.jsx
@@ -4,7 +4,7 @@ const AadharCard = ({ userData }) => {
   const { name, fathersName, dob, address, locality, pinCode, state, contact, uid } = userData;
 
   return (
-    <div style={{ border: '1px solid #000', padding: '20px', width: '300px', margin: '20px auto', backgroundColor: 'white' }}>
+    <div style={{ border: '1px solid #000', padding: '20px', width: '300px', margin: '20px auto', backgroundColor: 'white', color: 'black'}}>
       <h2>Aadhar Card</h2>
       <p><strong>UID:</strong> {uid}</p>
       <p><strong>Name:</strong> {name}</p>
